3be3a824e5ce2f50ef6bb3c3065747721d5973e5
fin10 <fender64@gmail.com>
2022-06-11 22:29:54 +0900

    공부 일지 기능 구현

--- github.py
+++ github.py
@@ -7,3 +7,10 @@ def get_issues():
     response = requests.get(url)
 
     return json.loads(response.text)
+
+
+def get_comments(issue_number: str):
+    url = 'https://api.github.com/repos/dnjsthdus/study_diary/issues/{number}/comments'.format(number=issue_number)
+    response = requests.get(url)
+
+    return json.loads(response.text)
--- main.py
+++ main.py
@@ -1,6 +1,51 @@
 import github
+from datetime import datetime
 
 issues = github.get_issues()
 
-print('title: {title}'.format(title=issues[0]['title']))
-print('body: {body}'.format(body=issues[0]['body']))
+
+def get_subject(issue: object):
+    labels = issue['labels']
+    for label in labels:
+        if label['name'].startswith('과목-'):
+            return label['name'][3:]
+
+    return None
+
+
+def get_study_time(issue: object):
+    comments = github.get_comments(issue['number'])
+    for comment in comments:
+        if comment['body'].startswith('공부시간-'):
+            return int(comment['body'][5:])
+
+    return 0
+
+
+def is_study_completed(issue: object):
+    labels = issue['labels']
+    for label in labels:
+        if label['name'] == '완료':
+            return True
+
+    return False
+
+
+total_study_time: int = 0
+
+for issue in issues:
+    study_time = get_study_time(issue)
+    total_study_time = total_study_time + study_time
+
+    print('---------------------')
+    print('제목: {title}'.format(title=issue['title']))
+    print('내용: {body}'.format(body=issue['body']))
+    print('공부 과목: {subject}'.format(subject=get_subject(issue)))
+    print('공부 시간: {time}'.format(time=study_time))
+    print('공부 완료: {state}'.format(state=is_study_completed(issue)))
+    print('---------------------')
+    print()
+
+print('[공부 요약]')
+print('날짜: {date}'.format(date=datetime.today().strftime('%Y년 %m월 %d일')))
+print('총 공부 시간: {time}'.format(time=total_study_time))
